3233516a00263bf45b62d834b6bde4549e15adf3
Gaya Bernou <bernou.gaya@gmail.com>
2014-12-10 14:57:18 +0100

    factorielle rec

---
+++ d05/my_factorielle_rec.c
@@ -0,0 +1,14 @@
+/*
+** By Gaya Bernou
+** Retourne N! réalisée de manière récursive.
+*/
+
+int     my_factorielle_rec(int n)
+{
+  if (n < 0 || n > 12)
+    return (0);
+  else if (n == 0)
+    return (1);
+  else
+    return (n * my_factorielle_rec(n - 1));
+}
